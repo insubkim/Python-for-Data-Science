cf0559960efa4353c8b82443664c35692a80ed88
insub <j.j.kant1948@gmail.com>
2024-11-14 22:14:33 +0900

    FEAT: ex01 done

--- Python - 0 - Starting/ex01/format_ft_time.py
+++ Python - 0 - Starting/ex01/format_ft_time.py
@@ -2,9 +2,10 @@ from time import time, gmtime
 from datetime import datetime
 
 current_time = time() # returns Epoche time
-print('Seconds since January 1, 1970: ', current_time)
-gmt_time = gmtime(current_time) # parses to year mon day min sec etc
-print(gmt_time)
-formatted_gmt = datetime(gmt_time.tm_year, gmt_time.tm_mon, gmt_time.tm_mon) # something useful?
-print(formatted_gmt)
-print(formatted_gmt.strftime("%B"))
+print('Seconds since January 1, 1970: ', current_time, ' or ', "%.2e"%current_time, ' in scientific notation')
+
+gmt_time = gmtime(current_time)
+
+formatted_gmt = datetime(gmt_time.tm_year, gmt_time.tm_mon, gmt_time.tm_mon)
+
+print(formatted_gmt.strftime("%B %d %Y"))
