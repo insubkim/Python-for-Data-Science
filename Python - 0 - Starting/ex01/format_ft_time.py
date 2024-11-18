d23e22b669a5e1c024dbb84b29733be40f62e287
insub <j.j.kant1948@gmail.com>
2024-11-18 20:22:59 +0900

    FIX: ex02

--- Python - 0 - Starting/ex01/format_ft_time.py
+++ Python - 0 - Starting/ex01/format_ft_time.py
@@ -5,7 +5,7 @@ current_time = time() # returns Epoche time
 print('Seconds since January 1, 1970: ', current_time, ' or ', "%.2e"%current_time, ' in scientific notation')
 
 gmt_time = gmtime(current_time)
-
-formatted_gmt = datetime(gmt_time.tm_year, gmt_time.tm_mon, gmt_time.tm_mon)
+help(gmtime)
+formatted_gmt = datetime(gmt_time.tm_year, gmt_time.tm_mon, gmt_time.tm_mday)
 
 print(formatted_gmt.strftime("%B %d %Y"))
