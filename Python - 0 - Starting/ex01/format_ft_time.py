feac7ffab7fa60b3a7e0dc0704816e17c4439499
insub <j.j.kant1948@gmail.com>
2024-11-24 17:19:05 +0900

    REFACTOR: ex00, ex01

--- Python - 0 - Starting/ex01/format_ft_time.py
+++ Python - 0 - Starting/ex01/format_ft_time.py
@@ -2,10 +2,10 @@ from time import time, gmtime
 from datetime import datetime
 
 current_time = time() # returns Epoche time
-print('Seconds since January 1, 1970: ', current_time, ' or ', "%.2e"%current_time, ' in scientific notation')
+print('Seconds since January 1, 1970: ', current_time, ' or ', "%.2e" % current_time, ' in scientific notation')
 
 gmt_time = gmtime(current_time)
-help(gmtime)
+# help(gmtime)
 formatted_gmt = datetime(gmt_time.tm_year, gmt_time.tm_mon, gmt_time.tm_mday)
 
 print(formatted_gmt.strftime("%B %d %Y"))
